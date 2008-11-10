e5650b15e79dde2c830371afc344f459af5242ed
Dustin Sallings <dustin@spy.net>
2008-11-09 17:07:35 -0800

    XML stream processing for friendfeed feed results.

---
+++ lib/ffxml.py
@@ -0,0 +1,145 @@
+from twisted.internet import error
+from twisted.web import sux
+
+class NoopParser(object):
+    def __init__(self, n):
+        self.name = n
+        self.done = False
+    def gotTagStart(self, name):
+        pass
+    def gotTagEnd(self, name, data):
+        self.done = (name == self.name)
+
+class BaseXMLHandler(object):
+
+    SIMPLE_PROPS = []
+    COMPLEX_PROPS = {}
+    tag_name = None
+
+    def __init__(self):
+        self.done = False
+        self.current_ob = None
+        for p in self.SIMPLE_PROPS:
+            self.__dict__[p] = None
+
+    def gotTagStart(self, name):
+        if self.current_ob:
+            self.current_ob.gotTagStart(name)
+        elif name in self.COMPLEX_PROPS:
+            self.current_ob = self.COMPLEX_PROPS[name]()
+        elif name in self.SIMPLE_PROPS:
+            pass
+        else:
+            self.current_ob = NoopParser(name)
+
+    def gotTagEnd(self, name, data):
+        if self.current_ob:
+            self.current_ob.gotTagEnd(name, data)
+            if self.current_ob.done:
+                if name in self.COMPLEX_PROPS:
+                    self.__dict__[name] = self.current_ob
+                self.current_ob = None
+        elif name == self.tag_name:
+            self.done = True
+        elif name in self.SIMPLE_PROPS:
+            self.__dict__[name] = data
+
+    def __repr__(self):
+        return "{%s %s}" % (self.tag_name, self.__dict__)
+
+class Via(BaseXMLHandler):
+
+    SIMPLE_PROPS = ['url', 'name']
+    tag_name = 'via'
+
+class Service(BaseXMLHandler):
+
+    SIMPLE_PROPS = ['profileUrl', 'iconUrl', 'id', 'entryType', 'name']
+    tag_name = 'service'
+
+class User(BaseXMLHandler):
+
+    SIMPLE_PROPS = ['profileUrl', 'nickname', 'id', 'name']
+    tag_name = 'user'
+
+class Comment(BaseXMLHandler):
+
+    SIMPLE_PROPS = ['body', 'is_new', 'date', 'id']
+    COMPLEX_PROPS = {'via': Via, 'user': User}
+    tag_name = 'comment'
+
+class Entry(BaseXMLHandler):
+
+    SIMPLE_PROPS = ['updated', 'title', 'is_new', 'link', 'anonymous',
+        'published', 'hidden', 'id']
+    COMPLEX_PROPS = {'via': Via, 'service': Service, 'comment': Comment,
+        'user': User}
+
+    def __init__(self):
+        super(Entry, self).__init__()
+        self.comments=[]
+
+    def gotTagEnd(self, name, data):
+        super(Entry, self).gotTagEnd(name, data)
+        if name == 'comment':
+            self.comments.append(self.comment)
+            del self.comment
+
+    tag_name = 'entry'
+
+class Feed(sux.XMLParser):
+
+    """A file-like thingy that parses a friendfeed feed with SUX."""
+    def __init__(self, delegate):
+        self.delegate=delegate
+
+        self.connectionMade()
+        self.currentEntry=None
+        self.data=[]
+
+        self.token=None
+        self.poll_interval=None
+        self.incomplete=None
+    def write(self, b):
+        self.dataReceived(b)
+    def close(self):
+        self.connectionLost(error.ConnectionDone())
+    def open(self):
+        pass
+    def read(self):
+        return None
+
+    # XML Callbacks
+    def gotTagStart(self, name, attrs):
+        self.data=[]
+        if name ==  'entry':
+            self.currentEntry = Entry()
+        elif self.currentEntry:
+            self.currentEntry.gotTagStart(name)
+
+    def gotTagEnd(self, name):
+        if name in ['token', 'poll_interval', 'incomplete']:
+            self.__dict__[name] = ''.join(self.data)
+        elif name == 'update':
+            self.delegate.gotUpdate(self.token, self.poll_interval,
+                self.incomplete)
+        elif name == 'entry':
+            self.currentEntry.done = True
+            self.delegate.gotEntry(self.currentEntry)
+        elif self.currentEntry:
+            self.currentEntry.gotTagEnd(name, ''.join(self.data).decode('utf8'))
+
+    def gotText(self, data):
+        self.data.append(data)
+
+    def gotEntityReference(self, data):
+        if data == 'quot':
+            self.data.append('"')
+        elif data == 'lt':
+            self.data.append('&lt;')
+        elif data == 'gt':
+            self.data.append('&gt;')
+        elif data == 'amp':
+            self.data.append('&amp;')
+        else:
+            print "Unhandled entity reference: ", data
---
+++ test/ffxml_test.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2008  Dustin Sallings <dustin@spy.net>
+"""
+
+import sys
+sys.path.append("lib")
+sys.path.append("../lib")
+
+import unittest
+
+import ffxml
+
+class XMLParserTest(unittest.TestCase):
+
+    def testParsing(self):
+        # This needs to do something useful...
+        ts=self
+        class D(object):
+            def gotUpdate(self, token, poll_interval, incomplete):
+                ts.assertEquals('39180875991', token)
+                ts.assertEquals('40', poll_interval)
+                ts.assertEquals('False', incomplete)
+            def gotEntry(self, e):
+                ts.assertEquals("2008-11-03T19:43:09Z", e.updated)
+                ts.assertEquals("Ken Sheppardson just committed a change to enjit on GitHub",
+                    e.title)
+                ts.assertEquals("True", e.is_new)
+                ts.assertEquals("http://github.com/kshep/enjit/commit/dd3ed5a5421e465e2e2dee8130032eefaff29bb8",
+                    e.link)
+                ts.assertEquals("False", e.anonymous)
+                ts.assertEquals("2008-11-03T19:43:09Z", e.published)
+                ts.assertEquals("False", e.hidden)
+                ts.assertEquals("0d751d07-a439-49a5-9c26-afda3d1fdea3", e.id)
+                # Via
+                ts.assertEquals("http://github.com/", e.via.url)
+                ts.assertEquals("GitHub", e.via.name)
+                # Service
+                ts.assertEquals("http://friendfeed.com/", e.service.profileUrl)
+                ts.assertEquals("http://chan.friendfeed.com/static/images/icons/internal.png?v=e471e9afdf04ae568dcbddb5584fc6c0",
+                    e.service.iconUrl)
+                ts.assertEquals("internal", e.service.id)
+                ts.assertEquals("link", e.service.entryType)
+                ts.assertEquals("FriendFeed", e.service.name)
+                # User
+                ts.assertEquals("http://chan.friendfeed.com/kshep",
+                    e.user.profileUrl)
+                ts.assertEquals("kshep", e.user.nickname)
+                ts.assertEquals("46958b7d-4bde-4890-8842-8cc33e8f55bd",
+                    e.user.id)
+                ts.assertEquals("Ken Sheppardson", e.user.name)
+                # Comment
+                c=e.comments[0]
+                ts.assertEquals("dd3ed5a5421e465e2e2dee8130032eefaff29bb8 - Fix for UnicodeEncodeError",
+                    c.body)
+                ts.assertEquals("True", c.is_new)
+                ts.assertEquals("2008-11-03T19:43:09Z", c.date)
+                ts.assertEquals("56ce52c7-a156-49ec-a826-128107e5a936",
+                    c.id)
+                # Comment via
+                ts.assertEquals("http://github.com/", c.via.url)
+                ts.assertEquals("GitHub", c.via.name)
+                # Comment user
+                ts.assertEquals("http://chan.friendfeed.com/kshep",
+                    c.user.profileUrl)
+                ts.assertEquals("kshep", c.user.nickname)
+                ts.assertEquals("46958b7d-4bde-4890-8842-8cc33e8f55bd",
+                    c.user.id)
+                ts.assertEquals("Ken Sheppardson", c.user.name)
+        ff = ffxml.Feed(D())
+
+        f=open("test/sample.xml")
+        d=f.read()
+        f.close()
+
+        ff.write(d)
+        f.close()
+
+    def testParsingMultiComment(self):
+        # This needs to do something useful...
+        ts=self
+        class D(object):
+            def gotUpdate(self, token, poll_interval, incomplete):
+                ts.assertEquals('39180875991', token)
+                ts.assertEquals('40', poll_interval)
+                ts.assertEquals('False', incomplete)
+            def gotEntry(self, e):
+                ts.assertEquals("2008-11-03T19:43:09Z", e.updated)
+                ts.assertEquals("Ken Sheppardson just committed a change to enjit on GitHub",
+                    e.title)
+                ts.assertEquals("True", e.is_new)
+                ts.assertEquals("http://github.com/kshep/enjit/commit/dd3ed5a5421e465e2e2dee8130032eefaff29bb8",
+                    e.link)
+                ts.assertEquals("False", e.anonymous)
+                ts.assertEquals("2008-11-03T19:43:09Z", e.published)
+                ts.assertEquals("False", e.hidden)
+                ts.assertEquals("0d751d07-a439-49a5-9c26-afda3d1fdea3", e.id)
+                # Via
+                ts.assertEquals("http://github.com/", e.via.url)
+                ts.assertEquals("GitHub", e.via.name)
+                # Service
+                ts.assertEquals("http://friendfeed.com/", e.service.profileUrl)
+                ts.assertEquals("http://chan.friendfeed.com/static/images/icons/internal.png?v=e471e9afdf04ae568dcbddb5584fc6c0",
+                    e.service.iconUrl)
+                ts.assertEquals("internal", e.service.id)
+                ts.assertEquals("link", e.service.entryType)
+                ts.assertEquals("FriendFeed", e.service.name)
+                # User
+                ts.assertEquals("http://chan.friendfeed.com/kshep",
+                    e.user.profileUrl)
+                ts.assertEquals("kshep", e.user.nickname)
+                ts.assertEquals("46958b7d-4bde-4890-8842-8cc33e8f55bd",
+                    e.user.id)
+                ts.assertEquals("Ken Sheppardson", e.user.name)
+
+                # Comment
+                c=e.comments[0]
+                ts.assertEquals("dd3ed5a5421e465e2e2dee8130032eefaff29bb8 - Fix for UnicodeEncodeError",
+                    c.body)
+                ts.assertEquals("True", c.is_new)
+                ts.assertEquals("2008-11-03T19:43:09Z", c.date)
+                ts.assertEquals("56ce52c7-a156-49ec-a826-128107e5a936", c.id)
+                # Comment via
+                ts.assertEquals("http://github.com/", c.via.url)
+                ts.assertEquals("GitHub", c.via.name)
+                # Comment user
+                ts.assertEquals("http://chan.friendfeed.com/kshep",
+                    c.user.profileUrl)
+                ts.assertEquals("kshep", c.user.nickname)
+                ts.assertEquals("46958b7d-4bde-4890-8842-8cc33e8f55bd",
+                    c.user.id)
+                ts.assertEquals("Ken Sheppardson", c.user.name)
+
+                # Second Comment
+                c=e.comments[1]
+                ts.assertEquals("another comment", c.body)
+                ts.assertEquals("True", c.is_new)
+                ts.assertEquals("2008-11-03T20:43:09Z", c.date)
+                ts.assertEquals("56ce52c7-a156-49ec-a826-128107e5a93x", c.id)
+                # Comment via
+                ts.assertEquals("http://github.com/2", c.via.url)
+                ts.assertEquals("GitHub 2", c.via.name)
+                # Comment user
+                ts.assertEquals("http://chan.friendfeed.com/dlsspy",
+                    c.user.profileUrl)
+                ts.assertEquals("dlsspy", c.user.nickname)
+                ts.assertEquals("46958b7d-4bde-4890-8842-8cc33e8f55bx",
+                    c.user.id)
+                ts.assertEquals("Dustin Sallings", c.user.name)
+        ff = ffxml.Feed(D())
+
+        f=open("test/sample-multi-comment.xml")
+        d=f.read()
+        f.close()
+
+        ff.write(d)
+        f.close()
+
+    def testSample2(self):
+        # This needs to do something useful...
+        ts=self
+        class D(object):
+            def gotUpdate(self, token, poll_interval, incomplete):
+                ts.assertEquals('39935774494', token)
+                ts.assertEquals('40', poll_interval)
+                ts.assertEquals('True', incomplete)
+            def gotEntry(self, e):
+                if e.id == 'e78e58c8-64a1-4462-a97c-891b27745789':
+                    ts.assertEquals("The Ladner Report: Amazing: Obama Helped Stranded Stranger 20 Years Ago",
+                        e.title)
+        ff = ffxml.Feed(D())
+
+        f=open("test/sample2.xml")
+        d=f.read()
+        f.close()
+
+        ff.write(d)
+        f.close()
+
+if __name__ == '__main__':
+    unittest.main()
