c6622c932ac3547197d616987e89050d077fad70
Dustin Sallings <dustin@spy.net>
2008-11-09 20:11:51 -0800

    Example program should allow a path to be specified.

--- example/ffwatch.py
+++ example/ffwatch.py
@@ -14,14 +14,20 @@ from twisted.internet import reactor, protocol, defer, task
 
 import twistedfriends
 
-print "Starting feed for", sys.argv[1]
-
 def gotMessage(msg):
     print "Got a message: ", msg.title
     for comment in msg.comments:
         print " + %s" % comment.body
 
-rlp = twistedfriends.RealtimeLongPoll(sys.argv[1], sys.argv[2], gotMessage)
+if len(sys.argv) > 3:
+    path = sys.argv[3]
+else:
+    path = "/home"
+
+print "Starting feed for", sys.argv[1], "from", path
+
+rlp = twistedfriends.RealtimeLongPoll(sys.argv[1], sys.argv[2], gotMessage,
+    path)
 
 loop = task.LoopingCall(rlp)
 loop.start(0, True)
